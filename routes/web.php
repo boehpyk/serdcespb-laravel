3163a610c89900edf3c549c51468f6a6e6bc1f86
Igor Bulygin <boehpyk@gmail.com>
2019-12-21 22:09:35 +0300

    [FIX] fixed routes

--- routes/web.php
+++ routes/web.php
@@ -99,6 +99,12 @@ Route::get('/gallery', 'GalleryController@index')->name('gallery.index');
 
 Auth::routes();
 
+Route::get('/logout', '\App\Http\Controllers\Auth\LoginController@logout');
+
+Route::get('/home', function () {
+    return redirect('index.page');
+});
+
 Route::get('/{slug}', 'TextController@show')->name('text.show');
 
 
